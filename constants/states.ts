776de6f44dc3196f4268b0b1f40f482f9637b794
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-02-01 23:17:47 +0300

    [dev] adds states and cities

---
+++ constants/states.ts
@@ -0,0 +1,64 @@
+export type State = {
+  name: string;
+  cities: string[];
+};
+// Todo: complete the list
+export const states: State[] = [
+  {
+    name: "Chandigarh",
+    cities: ["Chandigarh"],
+  },
+  {
+    name: "Chhattisgarh",
+    cities: [
+      "Ambikapur",
+      "Balod",
+      "Baloda Pazar",
+      "Bastar",
+      "Bemetara",
+      "Bhilai",
+      "Bilaspur",
+      "Chirmiri",
+      "Dhamtari",
+      "Durg",
+      "Jagdalpur",
+      "Kabirdham",
+      "Kanker",
+      "Kondagaon",
+      "Korba",
+      "Mahasamund",
+      "Mungeli",
+      "Raigarh",
+      "Raipur",
+      "Rajnandgaon",
+    ],
+  },
+  {
+    name: "Delhi",
+    cities: ["Delhi"],
+  },
+  {
+    name: "Haryana",
+    cities: ["Ambala", "Bahadurgarh"],
+  },
+  {
+    name: "Jharkhand",
+    cities: ["Bokaro Steel City", "Chakradharpur"],
+  },
+  {
+    name: "Madhya Pradesh",
+    cities: ["Anuppur", "Betul"],
+  },
+  {
+    name: "Maharashtra",
+    cities: ["Aurangabad", "Mumbai"],
+  },
+  {
+    name: "Punjab",
+    cities: ["Abohar", "Amritsar"],
+  },
+  {
+    name: "Rajasthan",
+    cities: ["Ajmer", "Alwar"],
+  },
+];
