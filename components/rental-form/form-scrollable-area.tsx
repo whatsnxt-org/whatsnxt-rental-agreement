041053fe1d6f0e522fbb02e883e3981b750cefd6
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-02-01 23:00:41 +0300

    [styles] fix top padding on desktops

--- components/rental-form/form-scrollable-area.tsx
+++ components/rental-form/form-scrollable-area.tsx
@@ -6,7 +6,7 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 const FormScrollableArea = ({ children }: { children: ReactNode }) => {
   return (
     <ScrollArea className="w-full h-[calc(100%-9.5rem)] overflow-hidden">
-      <div className="py-6 lg:max-w-[480px] mx-auto">{children}</div>
+      <div className="py-6 lg:py-0 lg:max-w-[480px] mx-auto">{children}</div>
     </ScrollArea>
   );
 };
