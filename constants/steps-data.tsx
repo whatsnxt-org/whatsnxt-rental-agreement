e597811449d1bd58cecfe76f1236cc439851ccf8
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-02-02 11:20:58 +0300

    [dev] fills steps-data titles and icons

--- constants/steps-data.tsx
+++ constants/steps-data.tsx
@@ -1,5 +1,8 @@
 import BasicDetails from "@/components/rental-form/steps/basic-details";
 import { MdOutlineAssignment } from "react-icons/md";
+import { TfiUser } from "react-icons/tfi";
+import { LiaUserAstronautSolid, LiaFileContractSolid } from "react-icons/lia";
+import { IoLocateOutline } from "react-icons/io5";
 
 export type StepData = {
   title: {
@@ -17,8 +20,51 @@ export const stepsData: StepData[] = [
       desktop: "Enter Basic Details",
     },
     icon: (
-      <MdOutlineAssignment className="w-6 h-6" color="hsl(var(--primary))" />
+      <MdOutlineAssignment
+        className="w-6 h-6"
+        // color="hsl(var(--primary))"
+      />
     ),
+    component: <BasicDetails />,
+  },
+  {
+    title: {
+      mobile: "Landlord Details",
+      desktop: "Enter Landlord Details",
+    },
+    icon: <TfiUser className="w-6 h-6" />,
+    component: <BasicDetails />,
+  },
+  {
+    title: {
+      mobile: "Tenant Details",
+      desktop: "Enter Tenant Details",
+    },
+    icon: <LiaUserAstronautSolid className="w-6 h-6" />,
+    component: <BasicDetails />,
+  },
+  {
+    title: {
+      mobile: "Property Details",
+      desktop: "Enter Property Details",
+    },
+    icon: <IoLocateOutline className="w-6 h-6" />,
+    component: <BasicDetails />,
+  },
+  {
+    title: {
+      mobile: "Contract Details",
+      desktop: "Enter Contract Details",
+    },
+    icon: <LiaFileContractSolid className="w-6 h-6" />,
+    component: <BasicDetails />,
+  },
+  {
+    title: {
+      mobile: "Item List",
+      desktop: "Enter Item List",
+    },
+    icon: <LiaFileContractSolid className="w-6 h-6" />,
     component: <BasicDetails />,
   },
 ];
