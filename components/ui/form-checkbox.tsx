7e79b15a77d0d4ae3343908124b46672227ffb1e
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-02-03 12:09:12 +0300

    [feat] adds form-checkbox ui component

---
+++ components/ui/form-checkbox.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Control, FieldValues, Path } from "react-hook-form";
+
+type FormCheckboxProps<T extends FieldValues> = {
+  control: Control<T, any>;
+  name: Path<T>;
+  label: string;
+};
+export const FormCheckbox = <T extends FieldValues>({
+  control,
+  name,
+  label,
+}: FormCheckboxProps<T>) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="flex flex-row items-start space-x-3 space-y-0">
+          <FormControl>
+            <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+          </FormControl>
+          <div className="space-y-1 leading-none">
+            <FormLabel className="cursor-pointer">{label}</FormLabel>
+          </div>
+        </FormItem>
+      )}
+    />
+  );
+};
