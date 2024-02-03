cb5a0c226cc2cd8260fd2fa81bbb8e737fb1d458
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-02-03 13:15:52 +0300

    [dev] adds extra shared validations schema

--- lib/validations/shared.ts
+++ lib/validations/shared.ts
@@ -1,7 +1,12 @@
 import { z } from "zod";
+import { isPositiveInt } from "@/lib/utils";
 
 export const requiredStringSchema = z.string().min(1, "Required");
 export const requiredEmailSchema = z.string().email("Invalid Email Format");
 export const requiredPhoneNoSchema = z.coerce
   .string()
   .min(10, "Invalid Phone Number");
+
+export const requiredIntSchema = z.coerce
+  .string()
+  .refine((value) => isPositiveInt(value), "Invalid value");
