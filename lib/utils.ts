922fe0524f618b0d8a64aa1b8f6d05d58125198f
Tarek El-Masri <tarek.elmasri201@gmail.com>
2024-02-03 13:14:43 +0300

    [feat] adds isPositiveInt helper method

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const isPositiveInt = (num?: string) =>
+  typeof num !== "undefined" &&
+  !isNaN(num as unknown as number) &&
+  !isNaN(parseInt(num)) &&
+  parseInt(num) > 0;
